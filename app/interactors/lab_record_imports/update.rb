dd91fd2e3eb10a7a4320b5d479aa7e11cc67c9d3
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2020-05-05 16:14:55 -0300

    Remove unnecessary opening when updating LabRecordImports
    By doing this, we avoid leaving an open file descriptor in
    the server process. The number of open files that can
    handle a process is limited.

--- app/interactors/lab_record_imports/update.rb
+++ app/interactors/lab_record_imports/update.rb
@@ -8,9 +8,7 @@ module LabRecordImports
         lab_record_import.rows_file.attach(params[:rows_file])
         lab_record_import.update!(patient_id_state: :pending)
 
-        # `open` operation inside parenthesis is performed to transform
-        # an ActionDispatch::Http::UploadedFile into a File
-        rows = open(params[:rows_file].open, 'r') { |file| JSON.load(file) }
+        rows = open(params[:rows_file].path, 'r') { |file| JSON.load(file) }
         rows.each_with_index do |row, index|
           lab_record_for_row(index).update(
             patient_id: row[patient_id_index]['w'],
