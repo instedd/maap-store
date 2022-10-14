3d4d7799c1b10faa6e7e59ad453e7adb2f3c3037
Matias Garcia Isaia <mgarcia@manas.tech>
2022-10-14 17:58:56 -0300

    Move CI to Github Actions
    So long, TravisCI - and thanks for all the fish!
    
    See #107

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 # This will load the script from this repository. Make sure to point to a specific commit so the build continues to work
 # event if breaking changes are introduced in this repository
-source <(curl -s https://raw.githubusercontent.com/manastech/ci-docker-builder/3fee09cce08175cfd76a246dd95112686939fb9c/build.sh)
+source <(curl -s https://raw.githubusercontent.com/manastech/ci-docker-builder/e53ee8c387a4e252bff57a8bb92e32f2b4230737/build.sh)
 
 # Prepare the build
 dockerSetup
